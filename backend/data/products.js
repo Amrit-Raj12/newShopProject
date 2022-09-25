eadf3250a38059bceaf0a782b5213edd500d08a4
Amrit-Raj12 <amrit.1224@gmail.com>
2022-09-25 17:59:57 +0530

    Dev - uploaded

---
+++ backend/data/products.js
@@ -0,0 +1,104 @@
+const products = [
+    {
+     name: 'Radhe Krishna',
+     image: '/images/paint4.jpg',
+     description:
+       'Radhe-Krishna',
+       brand: 'By SunShineCanteen',
+       category: 'SunShine',
+     price: 929.99,
+     countInStock: 5,
+     rating: 3,
+     numReviews: 12,
+   },
+  
+  {
+    name: 'Elepahant Painting',
+    image: '/images/paint3.jpg',
+    description:
+      'Elepanat Painting',
+      brand: 'By SunShineCanteen',
+      category: 'SunShine',
+    price: 399.99,
+    countInStock: 11,
+    rating: 5,
+    numReviews: 12,
+  },
+  {
+    name: 'Window Painting',
+    image: '/images/paint9.jpg',
+    description:
+      'Wall Window Painting',
+     brand: 'By SunShineCanteen',
+     category: 'SunShine',
+    price: 52.99,
+    countInStock: 5,
+    rating: 4.5,
+    numReviews: 10,
+  },
+  {
+    name: 'Red Temple',
+    image: '/images/paint7.jpg',
+    description:
+      'Temple in Red',
+     brand: 'By SunShineCanteen',
+     category: 'SunShine',
+    price: 49.99,
+    countInStock: 7,
+    rating: 3.5,
+    numReviews: 10,
+  },
+    
+    {
+     name: 'Girl Painting',
+     image: '/images/paint1.jpg',
+     description:
+       'Beautiful Girl in Red Dress',
+     brand: 'By SunShineCanteen',
+     category: 'SunShine',
+     price: 89.99,
+     countInStock: 10,
+     rating: 4.5,
+     numReviews: 12,
+   },
+   {
+     name: 'Natural Painting',
+     image: '/images/paint2.jpg',
+     description:
+       'Beautiful Painting',
+       brand: 'By SunShineCanteen',
+       category: 'SunShine',
+     price: 599.99,
+     countInStock: 7,
+     rating: 4.0,
+     numReviews: 8,
+   },
+      
+   {
+     name: 'Girl in Specs',
+     image: '/images/paint8.jpg',
+     description:
+       'Girl in red blue specs',
+       brand: 'By SunShineCanteen',
+       category: 'SunShine',
+     price: 29.99,
+     countInStock: 0,
+     rating: 4,
+     numReviews: 12,
+   },
+   {
+    name: 'Elepant with baby elepant',
+    image: '/images/paint10.jpg',
+    description:
+      'Elepant with baby elepant',
+     brand: 'By SunShineCanteen',
+     category: 'SunShine',
+    price: 44.99,
+    countInStock: 4,
+    rating: 3.7,
+    numReviews: 10,
+  },
+ 
+]
+
+export default products
